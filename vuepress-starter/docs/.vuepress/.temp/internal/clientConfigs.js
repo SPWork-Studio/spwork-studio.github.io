a203a8f963dea337e20b0a87feaa9e068a0405c2
Jeyzhen <jeyzhen812@gmail.com>
2024-06-19 09:25:28 +0000

    new file:   .github/workflows/deploy-docs.yml

---
+++ vuepress-starter/docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,17 @@
+import * as clientConfig0 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@_gjaififdt2vcgipdw6f6xzbk4m/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc_qakmtaoi5f566jjbcmbh6hvgle/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc.7_esvf2uf5iq2eihwfq2eazrv3ge/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc_yryl3u4wviiw5bytilkcerjat4/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig4 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc.7_rgbtdiuk5rmh5bctntd4v2r6ue/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig5 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc._k6ixinj2nhgc26leij4rlf7cae/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig6 from '/workspaces/spwork-team/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.36_vuepress@2.0.0-rc.13_@vuepress+bundler-vite@2.0.0-rc.7_@t_w7tnwol3kdsz3t4d7cji2nksoi/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+].map((m) => m.default).filter(Boolean)
